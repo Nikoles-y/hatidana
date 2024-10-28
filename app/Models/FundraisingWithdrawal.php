0e88a2a02ad4b7cfce7748be1b7b66e52d3daf90
Nikoles-y <nikoles365@gmail.com>
2024-10-28 15:30:55 +0700

    penambahan softdeletes pada tabel database dan model

--- app/Models/FundraisingWithdrawal.php
+++ app/Models/FundraisingWithdrawal.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FundraisingWithdrawal extends Model
 {
     //
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = [
         'fundraising_id', 'fundraiser_id', 'has_received', 'has_sent', 'amount_requested', 'proof', 'amount_received', 'bank_account_name', 'bank_name', 'bank_account_number',
     ];
