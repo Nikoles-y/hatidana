4fee5792cf88524fac843d541b321713fec8da91
Nikoles-y <nikoles365@gmail.com>
2024-10-28 15:25:21 +0700

    model

--- app/Models/Donatur.php
+++ app/Models/Donatur.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Donatur extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'name', 'notes', 'fundraising_Id', 'total_amount', 'is_paid', 'proof',
+    ];
 }
--- app/Models/FundraisingPhase.php
+++ app/Models/FundraisingPhase.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FundraisingPhase extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'name', 'notes', 'fundraising_id', 'photo',
+    ];
 }
