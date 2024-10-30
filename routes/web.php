b88524417534bf148d0bc189c5d9434ffe8019f9
Nikoles-y <nikoles365@gmail.com>
2024-10-30 15:14:58 +0700

    setup fitur apply fundraiser

--- routes/web.php
+++ routes/web.php
@@ -23,29 +23,31 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::prefix('admin')->name('admin.')->group(function() {
-        Route::resource('categories', CategoryController::class)->middleware('role:owner');
-        Route::resource('donaturs', DonaturController::class)->middleware('role:owner');
-        Route::resource('fundraisers', FundraiserController::class)->middleware('role:owner')->except('index');
-        Route::get('fundraisers', [FundraiserController::class , 'index'])->name('fundraisers.index');
-        Route::resource('fundraising_withdrawals', FundraisingWithdrawalController::class)->middleware('role:owner|fundraiser');
-        Route::post('fundraising_withdrawals/request/{fundraising:id}', [FundraisingWithdrawalController::class, 'store'])
-        ->middleware('role:fundraiser')
-        ->name('fundraising_withdrawal.store');
-        Route::resource('fundraising_phases', FundraisingPhaseController::class)->middleware('role:owner|fundraiser');
-        Route::post('fundraising_phases/update/{fundraising:id}', [FundraisingPhaseController::class, 'store'])
-        ->middleware('role:fundraiser')
-        ->name('fundraising_phases.store');
+        Route::prefix('admin')->name('admin.')->group(function() {
+            Route::resource('categories', CategoryController::class)->middleware('role:owner');
+            Route::resource('donaturs', DonaturController::class)->middleware('role:owner');
+            Route::resource('fundraisers', FundraiserController::class)->middleware('role:owner')->except('index');
+            Route::get('fundraisers', [FundraiserController::class , 'index'])->name('fundraisers.index');
+            Route::resource('fundraising_withdrawals', FundraisingWithdrawalController::class)->middleware('role:owner|fundraiser');
+            Route::post('fundraising_withdrawals/request/{fundraising:id}', [FundraisingWithdrawalController::class, 'store'])
+            ->middleware('role:fundraiser')
+            ->name('fundraising_withdrawal.store');
+            Route::resource('fundraising_phases', FundraisingPhaseController::class)->middleware('role:owner|fundraiser');
+            Route::post('fundraising_phases/update/{fundraising:id}', [FundraisingPhaseController::class, 'store'])
+            ->middleware('role:fundraiser')
+            ->name('fundraising_phases.store');
 
-        Route::resource('fundraisings', FundraisingController::class)->middleware('role:owner|fundraiser');
-        Route::post('fundraisings/active/{fundraising:id}', [FundraisingController::class, 'activate_fundraising'])
-        ->middleware('role:fundraiser')
-        ->name('fundraising_withdrawal.activate_fundraising');
+            Route::resource('fundraisings', FundraisingController::class)->middleware('role:owner|fundraiser');
+            Route::post('fundraisings/active/{fundraising:id}', [FundraisingController::class, 'activate_fundraising'])
+            ->middleware('role:fundraiser')
+            ->name('fundraising_withdrawal.activate_fundraising');
 
-        Route::post('/fundraiser/apply', [DashboardController::class, 'my_withdrawals'])->name('my-withdrawals');
-        Route::post('/my-withdrawals/details/{fundraisingWithdrawal}', [DashboardController::class, 'my_withdrawals'])->name('my-withdrawals.details');
-    
-    });
+            Route::post('/fundraiser/apply', [DashboardController::class, 'apply_fundraiser'])->name('fundraiser.apply');
+
+            Route::get('/my-withdrawals', [DashboardController::class, 'my_withdrawals'])->name('my-withdrawals');
+            Route::get('/my-withdrawals/details/{fundraisingWithdrawal}', [DashboardController::class, 'my_withdrawals'])->name('my-withdrawals.details');
+        
+        });
 });
 
 require __DIR__.'/auth.php';
